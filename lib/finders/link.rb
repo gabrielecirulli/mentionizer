68d4728eba89ce3dcf744d8e897a81cfc32c53d0
Stefano Verna <s.verna@cantierecreativo.net>
2014-04-03 13:07:39 +0200

    Link finder

--- lib/finders/link.rb
+++ lib/finders/link.rb
@@ -1,7 +1,34 @@
 require_relative './base'
+require 'uri'
 
 module Finders
   class Link < Base
+    VALID_SCHEMES = %w(http https)
+    VALID_DOMAINS = %w(twitter.com www.twitter.com)
+
+    def find
+      usernames.map do |username|
+        User.new(username)
+      end
+    end
+
+    def usernames
+      document.css('a').map do |element|
+        parse_username(element['href'])
+      end.compact.uniq
+    end
+
+    def parse_username(url)
+      uri = URI(url)
+      interesting = VALID_SCHEMES.include?(uri.scheme) &&
+                    VALID_DOMAINS.include?(uri.host)
+
+      if interesting
+        uri.path.gsub(/^\//, '')
+      end
+    rescue ArgumentError
+      nil
+    end
   end
 end
 
