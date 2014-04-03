e57393594aebd4717ad6f788c668df2869ee2622
Gabriele Cirulli <gabrycir@gmail.com>
2014-04-03 14:14:03 +0200

    Build share intent finder

--- lib/finders/share_intent.rb
+++ lib/finders/share_intent.rb
@@ -2,6 +2,19 @@ require_relative './base'
 
 module Finders
   class ShareIntent < Base
+    INTENT_URL = "https://twitter.com/share"
+
+    def find
+      if intent_links
+        intent_links.map { |link| User.new(link['data-via']) }
+      else
+        []
+      end
+    end
+
+    def intent_links
+      @intent_links ||= document.css("a[href='#{INTENT_URL}'][data-via]")
+    end
   end
 end
 
